e8028cd57f6e5f607c1470fa9e5c6fa9e0bd6796
kasho11 <kasho777@gmail.com>
2014-10-07 22:25:07 -0600

    Completion of Part 2

--- project.m
+++ project.m
@@ -16,4 +16,20 @@ y = format_coordinate(2,new_figure);
 axis_scale = get_axis_values(x,y);
 print_axis_scale(axis_scale);
 axis(axis_scale);
+%plot(x,y);
+mtx = load("~/Octave/Proj-1/affine.txt");
+i = 1;
+axiss = [mtx(3), mtx(5);mtx(4), mtx(6)]
+origins = [mtx(1);mtx(2)]
+test = size(var);
+fun = var;
+while i < test(1)
+fun (i, :) = transpose(origins+(axiss*transpose(fun(i, :))));
+fun (i, 2) = fun(i, 2)+1;
+i = i + 1;
+end
+fun
+x = format_coordinate(1, fun);
+y = format_coordinate(2, fun);
+axis(axis_scale);
 plot(x,y);
